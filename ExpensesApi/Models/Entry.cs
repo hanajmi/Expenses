11e5ad67d79ad42e58881c83fd0e075e2ef27511
Hassan <ha+najmi@outlook.com>
2019-06-05 01:10:55 +0430

    First Commit

---
+++ ExpensesApi/Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpensesApi.Models
+{
+    public class Entry
+    {
+        public string descriotion  { get; set; }
+        public bool is_expense { get; set; }
+        public double value { get; set; }
+    }
+}
